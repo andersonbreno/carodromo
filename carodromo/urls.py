75e82926ee8e9341dcfe6faded6e3d7d0aeb98ef
anderson.azevedo <andersonbrenotec@gmail.com>
2023-05-16 22:50:25 -0300

    Corrigi nome da rora aluno_6ano.

--- carodromo/urls.py
+++ carodromo/urls.py
@@ -5,7 +5,7 @@ from .views import index, aluno, aluno_add, aluno_6ano, aluno_7ano, aluno_8ano, 
 urlpatterns = [
     path('', index, name='index'),
     path('adicionar_aluno/', aluno_add, name='aluno_add'),
-    path('aluno_6ano/', aluno_6ano, name='aluno_6aluno'), 
+    path('aluno_6ano/', aluno_6ano, name='aluno_6ano'), 
     path('aluno_7ano/', aluno_7ano, name='aluno_7ano'), 
     path('aluno_8ano/', aluno_8ano, name='aluno_8ano'),
     path('aluno_9ano/', aluno_9ano, name='aluno_9ano'),
