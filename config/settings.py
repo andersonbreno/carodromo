a53d3b87f336b9825cb1e9219d67c45ab48ef5ec
anderson.azevedo <andersonbrenotec@gmail.com>
2023-05-18 15:09:33 -0300

    Consertei as configurações do .env e do sqlite.

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_static
+from dotenv import load_dotenv
+
+load_dotenv()
+# import dj_static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +83,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
