f52fb11b2acf24f9ac3a8a60fa8d59d8d65624c0
anderson.azevedo <andersonbrenotec@gmail.com>
2023-06-03 21:22:54 -0300

    Adicionar CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,10 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['web-production-ac35.up.railway.app/*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-ac35.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['web-production-ac35.up.railway.app']
 
 
 # Application definition
